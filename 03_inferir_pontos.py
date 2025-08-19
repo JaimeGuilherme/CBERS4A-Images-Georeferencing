0e126ef1b7629b368d3275a2183f5ac33fe450a0
JaimeGuilherme <jaime.guilherme67@gmail.com>
2025-08-19 12:34:01 -0300

    tqdm applying

--- 03_inferir_pontos.py
+++ 03_inferir_pontos.py
@@ -27,7 +27,9 @@ def inferir_modelo(model, dataloader, device, threshold):
 
 def extrair_pontos(preds_binarias, nomes_patches, pasta_imagens):
     dados = []
-    for pred, nome_patch in zip(preds_binarias, nomes_patches):
+    for pred, nome_patch in tqdm(zip(preds_binarias, nomes_patches), 
+                                 desc="Extraindo pontos", 
+                                 total=len(nomes_patches)):
         mask = pred.squeeze().astype(np.uint8)
         labeled, num_features = label(mask)
         if num_features == 0:
@@ -40,8 +42,12 @@ def extrair_pontos(preds_binarias, nomes_patches, pasta_imagens):
         for centro in centros:
             y, x = centro
             lon, lat = rasterio.transform.xy(patch_transform, int(round(y)), int(round(x)))
-            dados.append({'geometry': Point(lon, lat), 'patch': nome_patch,
-                          'x_pixel': int(round(x)), 'y_pixel': int(round(y))})
+            dados.append({
+                'geometry': Point(lon, lat),
+                'patch': nome_patch,
+                'x_pixel': int(round(x)),
+                'y_pixel': int(round(y))
+            })
     gdf = gpd.GeoDataFrame(dados)
     if len(dados) > 0:
         gdf.set_crs(crs, inplace=True)
@@ -68,7 +74,9 @@ if __name__ == '__main__':
 
     results_bin, nomes_patches = inferir_modelo(model, test_loader, DEVICE, threshold)
 
-    for pred, nome_patch in zip(results_bin, nomes_patches):
+    for pred, nome_patch in tqdm(zip(results_bin, nomes_patches),
+                                total=len(nomes_patches),
+                                desc="Salvando máscaras"):
         mask_patch = (pred[0, 0] > 0).astype(np.uint8) * 255
         patch_path = os.path.join(caminho_test_img, nome_patch)
         with rasterio.open(patch_path) as src:
